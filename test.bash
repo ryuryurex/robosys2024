d8f1ccab99d2fac5eccb0f1eb44473789f4241ac
Ryusei Noda <s23c1107nw@s.chibakoudai.jp>
2024-10-31 13:09:59 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -4,11 +4,20 @@ ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
-
 res=0
 
+### nomal input ###
 out=$(seq 5 | ./plus)
 [ "$out" = 15 ] || ng "$LINENO"
+
+### strange input ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit "$res"
