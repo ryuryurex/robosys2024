8d15a2fbab1e4795146ca211138b3f7ac81662a6
Ryusei Noda <s23c1107nw@s.chibakoudai.jp>
2024-11-29 23:25:45 +0900

    test

--- pass_test.bash
+++ pass_test.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ng () {
-    echo ${1}行目が違うよ
-    res=1
+        echo ${1}行目が違うよ
+        res=1
 }
 
 res=0
@@ -10,24 +10,29 @@ res=0
 ### 正常な入力のテスト ###
 out=$(echo 5 | ./passgenerater)
 [ $? = 0 ] || ng "$LINENO"
+[[ "$out" == "生成したパスワード: "* ]] || ng "$LINENO"
 
 out=$(echo 10 | ./passgenerater)
 [ $? = 0 ] || ng "$LINENO"
+[[ "$out" == "生成したパスワード: "* ]] || ng "$LINENO"
 
 ### 異常な入力のテスト ###
 out=$(echo -1 | ./passgenerater)
 [ $? = 1 ] || ng "$LINENO"
+[[ "$out" == "1以上の正数を入力してください" ]] || ng "$LINENO"
 
 out=$(echo 0 | ./passgenerater)
 [ $? = 1 ] || ng "$LINENO"
+[[ "$out" == "1以上の正数を入力してください" ]] || ng "$LINENO"
 
 out=$(echo あ | ./passgenerater)
 [ $? = 1 ] || ng "$LINENO"
+[[ "$out" == "無効な入力です" ]] || ng "$LINENO"
 
 out=$(echo | ./passgenerater)
 [ $? = 1 ] || ng "$LINENO"
+[[ "$out" == "入力がありません" ]] || ng "$LINENO"
 
 ### テスト結果 ###
 [ "$res" -eq 0 ] && echo OK
 exit "$res"
-
